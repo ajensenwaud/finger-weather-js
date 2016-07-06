76534ee178a42517e025c6d18adcec64cc226741
Anders Jensen-Waud <anders@jensenwaud.com>
2016-07-06 20:29:05 +1000

    Add switches for TCP port and listener address

--- weatherserver.js
+++ weatherserver.js
@@ -5,15 +5,21 @@ var util = require('util');
 var server = net.createServer(function(socket) { 
   console.log(socket.remoteAddress + ":" + socket.remotePort + " connected!");
   socket.pipe(socket);  
-  socket.on('data', function(data) { 
-		var location = ('' + data).trim();
-		var weather = yw.getCurrent(location, function(error, response, body) { 
-			var b = JSON.parse(body);
-			var condition = b.query.results.channel.item.condition;
-			// console.log('weater in ' + location + ' is: ' + condition.temp + ' ' + condition.text);
-			socket.write('Weather in ' + location + ' is ' + condition.text + ' with a temperature of ' + condition.temp + 'F\r\n');
-		});
+	socket.on('data', function(data) { 
+		if (data !== undefined && data.length > 0) {
+			var location = ('' + data).trim();
+			var weather = yw.getCurrent(location, function(error, response, body) { 
+				var b = JSON.parse(body);
+				var condition = b.query.results.channel.item.condition;
+				socket.write('Weather in ' + location + ' is ' + condition.text + ' with a temperature of ' + condition.temp + 'F\r\n');
+			});
+		} else if (data.length == 0) { 
+			console.log('client sent an empty request -- ignoring'); 
+		} else { 
+			console.log('client sent an undefined request -- ignoring');
+		}
   });
 }); 
-
-server.listen(1079, '127.0.0.1');
+var TCP_PORT = 1079; 
+var TCP_ADDRESS = '127.0.0.1';
+server.listen(TCP_PORT, TCP_ADDRESS);
